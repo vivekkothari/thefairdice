39b3e3e9653c243ad9840b103c391da853068ea8
vivekkothari <vivekkothari2304@gmail.com>
2013-10-13 11:15:29 +0530

    Config Changes

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpressv2');
+define('DB_NAME', 'tempTFD');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'thefairdice');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'Happyjoey2304');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ojybipwcea.database.windows.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
